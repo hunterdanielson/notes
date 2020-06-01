dfa245b8a09e84c29148680ab7bc1cdf7721507b
Hunter Danielson <hunterleedanielson@gmail.com>
2020-06-01 16:54:33 -0700

    working with functions

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const { parse, valid } = require('./lib/input.js');
 const { execute } = require('./lib/notes.js');
 
 let stuff = parse(process.argv);
+let response;
+
 if(valid(stuff)) {
-  execute(stuff);
+  response = execute(stuff);
 }
 
-
+console.log(response);
 console.log(stuff);
