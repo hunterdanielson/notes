83e138bacb88266df947e5ee0a30f5ecc451d1a2
Hunter Danielson <hunterleedanielson@gmail.com>
2020-06-04 09:15:13 -0700

    got rid of express code

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
 
 const { Input } = require('./lib/Input.js');
@@ -9,48 +7,15 @@ const Note = require('./lib/Note.js');
 const noteObject = new Input(process.argv);
 console.log(noteObject);
 
-if(Input.valid(noteObject)) Note.execute(noteObject);
+if(Input.valid(noteObject)) { 
+  Note.execute(noteObject)
+    .then(() => mongoose.connection.close());
+} else {
+  console.log('error');
+}
 
 // not sure what this does
 mongoose.connect('mongodb://localhost:27017/play', {
   useNewUrlParser: true,
   useUnifiedTopology: true
-});
-
-app.use(express.json());
-
-// Note.execute(noteObject);
-
-// need to make all these paths somehow passed from command line argv
-// make a note
-app.post('/notes', (req, res) => {
-  Note
-    .execute(req.body)
-    .then(note => res.send(note));
-});
-  
-// get all notes
-app.get('/notes', (req, res) => {
-  Note
-    .find()
-    .then(notes => res.send(notes));
-});
-  
-// get a specific note
-app.get('/notes/:id', (req, res) => {
-  Note
-    .findById(req.params.id)
-    .then(note => res.send(note));
-});
- 
-// delete a note by id
-app.delete('/notes/:id', (req, res) => {
-  Note
-    .findByIdAndDelete(req.params.id)
-    .then(note => res.send(note));
-});
-  
-// port 3000 best port
-app.listen(3000, () => {
-  console.log('Started on 3000');
 });
