ce631018aa0db287eab2dec368cfb51cf8776857
Hunter Danielson <hunterleedanielson@gmail.com>
2020-06-03 18:21:28 -0700

    added console logs for verification

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const mongoose = require('mongoose');
 const { Input } = require('./lib/Input.js');
 const Note = require('./lib/Note.js');
 
+// make a new input class with command line arguements parsed in
+const noteObject = new Input(process.argv);
+console.log(noteObject);
+
+if(Input.valid(noteObject)) Note.execute(noteObject);
+
+// not sure what this does
 mongoose.connect('mongodb://localhost:27017/play', {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -12,6 +19,9 @@ mongoose.connect('mongodb://localhost:27017/play', {
 
 app.use(express.json());
 
+// Note.execute(noteObject);
+
+// need to make all these paths somehow passed from command line argv
 // make a note
 app.post('/notes', (req, res) => {
   Note
